7c65ea9d7613b52b79c50c76cd75e50404cf874e
summermoved0n <135251802+summermoved0n@users.noreply.github.com>
2023-12-26 21:21:53 -0500

    add local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,16 +6,27 @@ import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
 import css from './App.module.css';
 
+const LS_KEY = 'phonebook';
+
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const savedState = JSON.parse(localStorage.getItem(LS_KEY));
+    if (savedState) {
+      this.setState({ contacts: savedState });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    const { contacts } = this.state;
+    if (prevState.contacts !== contacts) {
+      localStorage.setItem(LS_KEY, JSON.stringify(contacts));
+    }
+  }
 
   addContact = data => {
     const { contacts } = this.state;
